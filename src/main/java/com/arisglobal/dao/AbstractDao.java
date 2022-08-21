10d35e2b8588bf0d57a35ae8b1d432215bb43f57
starfighter2k <42906646+starfighter2k@users.noreply.github.com>
2022-08-21 09:54:05 +0530

    Add files via upload

---
+++ src/main/java/com/arisglobal/dao/AbstractDao.java
@@ -0,0 +1,25 @@
+package com.arisglobal.dao;
+
+import org.hibernate.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class AbstractDao {
+	@Autowired
+	private SessionFactory sessionFactory;
+	protected Session getSession()
+	{
+		return sessionFactory.getCurrentSession();
+	}
+	public void persist(Object entity)
+	{
+		getSession().persist(entity);
+	}
+	public void delete(Object entity)
+	{
+		getSession().delete(entity);
+	}
+	public void update(Object object)
+	{
+		getSession().update(object);
+	}
+}
