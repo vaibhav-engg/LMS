8d9a16d3ac09d996f65cbcfaa66ee5db4a0f9d6a
starfighter2k <42906646+starfighter2k@users.noreply.github.com>
2022-08-22 12:00:44 +0530

    Add files via upload

--- src/main/java/com/arisglobal/controller/UserController.java
+++ src/main/java/com/arisglobal/controller/UserController.java
@@ -3,7 +3,9 @@ package com.arisglobal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -56,4 +58,44 @@ public class UserController {
 	{
 		return "manageuser";
 	}
+	@RequestMapping("/delete")
+	public String redirectDelete()
+	{
+		return "delete";
+	}
+	@RequestMapping("/edit/{id}")
+	public String redirectUserDetailsById(@PathVariable("id")int id,Model m)
+	{
+		User res = userService.findById(id);
+		m.addAttribute("user", res);
+		return "edituserdetails";
+	}
+	@RequestMapping("/deleteuser")
+	public String redirectDeleteUser(@ModelAttribute("user") User user,Model model)
+	{
+		User res = userService.findById(user.getId());
+		user.setUser_name(res.getUser_name());
+		user.setAddress(res.getAddress());
+		user.setEmail(res.getEmail());
+		user.setMobile_number(res.getMobile_number());
+		user.setPassword(res.getPassword());
+		user.setRole_id(res.getRole_id());
+		return "deleteuser";
+	}
+	@RequestMapping("/deleteuser/{id}")
+	public String redirectDeleteUserById(@PathVariable("id")int id,Model m)
+	{
+		User res = userService.findById(id);
+		m.addAttribute("user", res);
+		return "deleteuser";
+	}
+	@RequestMapping("/deleteuser/confirmdeleteuser")
+	public String redirectConfirmDelete(@ModelAttribute("user")User user,Model m)
+	{
+		User res = userService.findById(user.getId());
+		userService.deleteUserByID(user.getId());
+		m.addAttribute("user", res);
+		return "deleteduser";
+	}
+	
 }
