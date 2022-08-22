654f35bf8658f01107a936361d5d55654cc9169c
starfighter2k <42906646+starfighter2k@users.noreply.github.com>
2022-08-22 14:24:44 +0530

    Update TransactionService.java

--- src/main/java/com/arisglobal/service/TransactionService.java
+++ src/main/java/com/arisglobal/service/TransactionService.java
@@ -2,13 +2,13 @@ package com.arisglobal.service;
 
 import java.util.ArrayList;
 
-import com.arisglobal.entity.LMSTransaction;
+import com.arisglobal.entity.Transaction;
 
 public interface TransactionService {
-	public LMSTransaction getTransactionById(int id);
+	public Transaction getTransactionById(int id);
 	public ArrayList<LMSTransaction> getAllCategories();
-	public void saveTransaction(LMSTransaction Transaction);
-	public void deleteTransaction(LMSTransaction Transaction);
-	public void updateTransaction(LMSTransaction Transaction);
-	public ArrayList<LMSTransaction> getUserTransaction(int lms_user_id);
+	public void saveTransaction(Transaction Transaction);
+	public void deleteTransaction(Transaction Transaction);
+	public void updateTransaction(Transaction Transaction);
+	public ArrayList<Transaction> getUserTransaction(int lms_user_id);
 }
