76928f61b7847fedc3c3ae8a612c92e99434cb00
Suman vaibhav <83916016+vaibhav-engg@users.noreply.github.com>
2022-08-22 14:05:49 +0530

    Add files via upload

--- src/main/java/com/arisglobal/controller/BorrowController.java
+++ src/main/java/com/arisglobal/controller/BorrowController.java
@@ -2,26 +2,34 @@ package com.arisglobal.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.arisglobal.dao.impl.TransactionDaoImpl;
 import com.arisglobal.entity.Transaction;
+import com.arisglobal.service.TransactionService;
 @Controller
 public class BorrowController {
-	@RequestMapping(value="/borrow/{asset_id}")
-	public String borrower(@PathVariable("asset_id") int asset_id, Model m) {
+	@Autowired
+	private TransactionService transactionService;
+//	private 
+	@RequestMapping(value="/borrow/{asset_id}/{lms_user_id}")
+	public String borrower(@PathVariable("asset_id") int asset_id,@PathVariable("lms_user_id") int lms_user_id, Model m ) {
 		Transaction transaction = new Transaction();
-		TransactionDaoImpl obj = new TransactionDaoImpl();
+//		TransactionDaoImpl obj = new TransactionDaoImpl();
 		Date date = new Date();
 		long ltime = date.getTime()+15*24*60*60*1000;
 		Date due_date = new Date(ltime);
 		transaction.setAsset_id(asset_id);
 		transaction.setDue_date((java.sql.Date) due_date);
 		transaction.setIssue_date((java.sql.Date)date);
-		obj.persist(transaction);
+		transaction.setLms_user_id(lms_user_id);
+		
+		
+		//transactionService.updateTransaction(trans);
+		transactionService.saveTransaction(transaction);
 		String msg = "Borrowed Successfully";
 		m.addAttribute("message", msg);
 		return "assetlist";
