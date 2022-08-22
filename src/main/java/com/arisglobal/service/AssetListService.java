40aba5a3a250dbdd91dfd285fcc6eb2b555a56c1
SHIVA KUMAR <57231596+shivashakya@users.noreply.github.com>
2022-08-22 12:44:11 +0530

    created service layer

---
+++ src/main/java/com/arisglobal/service/AssetListService.java
@@ -0,0 +1,18 @@
+package com.arisglobal.service;
+
+import java.util.ArrayList;
+
+import com.arisglobal.entity.Asset;
+
+public interface AssetListService {
+	
+	public void saveAsset(Asset asset);
+
+	public void deleteAsset(int asset_id);
+
+	public void updateAsset(Asset asset);
+
+	public Asset getAssetById(int asset_id);
+
+	public ArrayList<Asset> getAllAsset();
+}
